3f2fbb5341accf576bcfdd5d7591f94e61d5b2b5
John Akhilomen <jonbasanti@gmail.com>
2020-12-17 18:32:25 -0600

    Msg

--- src/app.js
+++ src/app.js
@@ -19,17 +19,16 @@ app.use(helmet())
 app.use(cors())
 
 
-/*app.use(function validateBearerToken(req, res, next) {
+app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         logger.error(`Unauthorized request to path: ${req.path}`);  
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     // move to the next middleware
     next()
-})*/
+})
   
 app.use("/api", indexRouter)
 app.use("/api/bookmarks", bookmarksRouter)
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,12 +1,17 @@
 const {expect} = require('chai')
+//const { token } = require('morgan')
 const supertest = require('supertest')
 const app = require('../src/app')
 const { bookmarks } = require('../src/store')
 
+const token = "myToken";
+
 describe('App', ()=>{
+    
     it('GET /api responds with 200 containing "hello world!"', ()=> {
         return supertest(app)
            .get('/api')
+           .set('Authorization', 'Bearer ' + token)
            .expect(200, 'hello world!')
     })
 })
@@ -16,6 +21,7 @@ describe('App', ()=>{
     it('GET /api/bookmarks responds with 200 containing "hello world!"', ()=> {
         return supertest(app)
            .get('/api/bookmarks')
+           .set('Authorization', 'Bearer ' + token)
            .expect(200, bookmarks)
     })
 })
@@ -25,6 +31,7 @@ describe('App', ()=>{
     it('GET /api/bookmarks/1 responds with 200 containing "hello world!"', ()=> {
         return supertest(app)
            .get('/api/bookmarks/1')
+           .set('Authorization', 'Bearer ' + token)
            .expect(200, bookmarks[0])
     })
 })
